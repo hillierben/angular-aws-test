bdcf8856649b2b00ebde9eb822933b50e1e1a91f
Ben.Hillier <ben.hillier@room58.com>
2025-06-23 20:27:52 +0100

    hhh

--- dist/angular-thing/server/angular-app-manifest.mjs
+++ dist/angular-thing/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 496, hash: '46ff568d3d2a66f74b3127f94a094c21ab1a0fc1bf6d27e3714361a0c7e0d840', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1009, hash: '5221e9643ad55bc4fa99c7c85ac932986039c431c7affffbdfb0475d0590bc12', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20850, hash: '85ba469f1587d733e09f263a7dcc1926aac1d8ea599e2c410ab1d56ecc8c9478', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 496, hash: '9006950ed0e47985c0a328603cf2d446fc511eed848cb24e6b287f9ccb4e50ad', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1009, hash: 'd6b247f334ea4715963dfbe12221cfbdb4f65da56d917104ef60d54853d9299c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 20855, hash: 'db8fc3818bdb6676f8b8338b8b77884784cbc9ea5d688c2b6aa036714d0de8b9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AWS Test has happened';
+  title = 'AWS Test has happened here';
 }
