dee05d3d2b85b9c89ffa82b0df5d74b00fe6b508
Ben.Hillier <ben.hillier@room58.com>
2025-06-23 20:24:31 +0100

    update

--- dist/angular-thing/server/angular-app-manifest.mjs
+++ dist/angular-thing/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 496, hash: '28bce8ae32635759a3d3934ec0cd72a467be6afa09e993179bc238bbc589554a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1009, hash: '409eb870770e881ea4734b4457d279f1b3182770799aaf60ddb5c3bd39bbdad0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20845, hash: '354b774c75faa8e0342ad697d43fbdb37b08274f29a43deaa3356c599bdf50f6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 496, hash: '46ff568d3d2a66f74b3127f94a094c21ab1a0fc1bf6d27e3714361a0c7e0d840', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1009, hash: '5221e9643ad55bc4fa99c7c85ac932986039c431c7affffbdfb0475d0590bc12', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 20850, hash: '85ba469f1587d733e09f263a7dcc1926aac1d8ea599e2c410ab1d56ecc8c9478', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
